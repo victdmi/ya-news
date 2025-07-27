925abb841555ebf46b9ba31b48aea686f52c022e
Victor Dmitruk <v.dmitruk.01@mail.ru>
2025-07-27 13:55:42 +0300

    Adds test_detail_page

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -3,13 +3,22 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
+from news.models import News
+
 
 class TestRoutes(TestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+
+
     def test_home_page(self):
-        # Вместо прямого указания адреса 
-        # получаем его при помощи функции reverse().
         url = reverse('news:home')
         response = self.client.get(url)
-        # Проверяем, что код ответа равен статусу OK (он же 200).
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_detail_page(self):
+        url = reverse('news:detail', kwargs={'pk': self.news.pk})
+        response = self.client.get(url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
